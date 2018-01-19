45f225fac8ddc7fcd8eaaf9eebba9c36dae32fe2
kebsont <mktapha@gmail.com>
2018-01-19 17:47:26 +0100

    add pi hostname server in settings

--- LiveObjectsSuivi/settings.py
+++ LiveObjectsSuivi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=lerf@(kdba=3bm7125@ory%7c#0g(s#gre390qe)k#3efn^zk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.10', '127.0.0.1']
+ALLOWED_HOSTS = ['192.168.1.10', '127.0.0.1', 'kebson.hopto.org']
 
 
 # Application definition
