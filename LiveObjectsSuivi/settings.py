d510a772b9105200c990244a89dfd5b65f932acf
kebsont <mktapha@gmail.com>
2018-01-19 15:17:40 +0000

    add hosts on seetings

--- LiveObjectsSuivi/settings.py
+++ LiveObjectsSuivi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=lerf@(kdba=3bm7125@ory%7c#0g(s#gre390qe)k#3efn^zk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.10']
 
 
 # Application definition
